786c1b118f9bb7b6c667318f4cb9ae13906183eb
Wes0808 <jaawbs@gmail.com>
2026-09-10 15:00:26 -0500

    FInishing up the Integrating Email JS Section

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ MCjPEhF1uQPmKkpIg*/
 
 function contact(event) {
     event.preventDefault();
-    /*emailjs
+    const load = document.querySelector('.modal__overlay--load');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible';
+
+    emailjs
         .sendForm(
             'service_1upo89r',
             'template_ay34op1',
             event.target,
             'MCjPEhF1uQPmKkpIg'
         ).then(() => {
-            
-        })*/
-    
-    const load = document.querySelector('.modal__overlay--load')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible";
-
-    setTimeout(() => {
-        load.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-    }, 1000);
+            load.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            load.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. please contact me directly on guywes1979@gmail.com"
+            );
+        })
 }
     
